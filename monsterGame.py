85772b50efac1d2400ff247fa03bf2a7011a5ef5
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-02-21 11:19:15 -0500

    first post monster chase

---
+++ monsterGame.py
@@ -0,0 +1,105 @@
+import pygame
+import random
+import time
+
+
+def main():
+    width = 510
+    height = 480
+    blue_color = (97, 159, 182)
+
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('My Game')
+    clock = pygame.time.Clock()
+
+    # Game initialization
+    background_image = pygame.image.load('images/background.png').convert_alpha()
+    hero_image = pygame.image.load('images/hero.png').convert_alpha()
+    
+    
+    class Monster():
+        def __init__(self):
+            self.image = pygame.image.load('images/monster.png').convert_alpha()
+            self.locationx = 400
+            self.locationy = 400
+            self.counter = 0
+        def location_jump(self):
+            if self.locationx == width:
+                self.locationx = 0
+            elif self.locationx == 0:
+                self.locationx = width
+            elif self.locationy == height:
+                self.locationy = 0
+            elif self.locationy == 0:
+                self.locationy = height
+        
+        def location_change(self):
+        
+            if self.counter <= 100:
+                self.locationx += 5
+                self.counter += 1
+            elif self.counter <= 200:
+                self.locationx -= 5
+                self.counter += 1
+            elif self.counter <= 300:
+                self.locationy += 5
+                self.counter += 1
+            elif self.counter <= 400:
+                self.locationy -= 5
+                self.counter += 1
+            elif self.counter <= 500:
+                self.locationy += 5
+                self.locationx += 5
+                self.counter += 1
+            elif self.counter <= 600:
+                self.locationy -= 5
+                self.locationx -= 5
+                self.counter += 1
+            elif self.counter <= 700:
+                self.locationy += 5
+                self.locationx -= 5
+                self.counter += 1
+            elif self.counter <= 800:
+                self.locationy -= 5
+                self.locationx += 5
+                self.counter += 1
+            else:
+                self.counter = 0
+    monster = Monster()
+    
+
+    stop_game = False
+    while not stop_game:
+        for event in pygame.event.get():
+
+            # Event handling
+
+            if event.type == pygame.QUIT:
+                stop_game = True
+
+
+        # Game logic
+        monster.location_jump()
+        monster.location_change()
+
+
+        # Draw background
+
+        # screen.fill(blue_color)
+
+        # Game display
+        screen.blit(background_image, (0, 0))
+        screen.blit(hero_image, (240, 240))
+        screen.blit(monster.image, (monster.locationx, monster.locationy))
+
+        
+
+
+        pygame.display.update()
+        clock.tick(60)
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
